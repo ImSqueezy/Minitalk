727a57f0a69cde8b44a9e20a4e2d15548b26c31d
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-02-11 13:37:12 +0100

    lazy to re-copy in the school's computer

---
+++ testing.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+void sighandler(int);
+
+int main () {
+   signal(SIGINT, sighandler);
+
+   while(1) {
+      printf("Going to sleep for a second...\n");
+      sleep(1); 
+   }
+   return(0);
+}
+
+void sighandler(int signum) {
+   printf("Caught signal %d, coming out...\n", signum);
+   exit(1);
+}
