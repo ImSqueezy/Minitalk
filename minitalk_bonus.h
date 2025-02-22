3e95c7b645e3ff1dc12970ba6656b86d38eabbd3
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-02-22 22:24:12 +0100

    bonus's header file

---
+++ minitalk_bonus.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aouaalla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/20 14:00:33 by aouaalla          #+#    #+#             */
+/*   Updated: 2025/02/20 14:00:34 by aouaalla         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# define ONE_BIT_MASK 1
+# define FIVE_BIT_MAST 5
+# include <signal.h>
+# include "printf/ft_printf.h"
+# include "Libft/libft.h"
+
+#endif
