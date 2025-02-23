97f9f6473795734500375cbc39150927c4064d96
ImSqueezy <ouaallaabdelali1@gmail.com>
2025-02-23 14:41:16 +0000

    modified .gitignore

--- client_bonus.c
+++ client_bonus.c
@@ -11,105 +11,25 @@
 /* ************************************************************************** */
 
 #include "minitalk_bonus.h"
-// void	data_sender(int pid, unsigned char octet);
 
-// int	g_flag;
-
-// int	arg_check(int ac, char **av)
-// {
-// 	if (ac != 3)
-// 	{
-// 		ft_printf("Invalid number of args!\n(Syntax: %s process_id message)\n",
-// 		av[0]);
-// 		return (0);
-// 	}
-// 	if (!av[1] || !av[2])
-// 		return (ft_printf("The message might be empty!"), 0);
-// 	return (ft_atoi(av[1]));
-// }
-
-// void	handler(int sig)
-// {
-// 	if (sig == SIGUSR1)
-// 		g_flag = 0;
-// 	else
-// 	{
-// 		ft_printf("The message has been delivered successfully!");
-// 		exit(0);
-// 	}
-// }
-
-// void	handler_initializer(struct sigaction *sig, int pid, char *message)
-// {
-// 	int	byte;
-
-// 	sig->sa_flags = 0;
-// 	sig->sa_handler = handler;
-// 	if (sigaction(SIGUSR1, sig, NULL) == -1)
-// 		return (ft_printf("The handler failed to be initialized!"), exit(0));
-// 	if (sigaction(SIGUSR2, sig, NULL))
-// 		return (ft_printf("The handler failed to be initialized!"), exit(0));
-// 	byte = -1;
-// 	while (message[++byte])
-// 		data_sender(pid, message[byte]);
-	
-// }
-
-// void	data_sender(int pid, unsigned char octet)
-// {
-// 	int		i;
-
-// 	i = 7;
-// 	while (i >= 0)
-// 	{
-// 		while (g_flag)
-// 			usleep(1);
-// 		g_flag = 1;
-// 		if ((octet >> i) & ONE_BIT_MASK)
-// 		{
-// 			if (kill(pid, SIGUSR1) != 0)
-// 				return (ft_printf("The pid is incorrect!\n"), exit(0));
-// 			else
-// 				ft_printf("1");
-// 		}
-// 		else
-// 		{
-// 			if (kill(pid, SIGUSR2) != 0)
-// 				return (ft_printf("The pid is incorrect!\n"), exit(0));
-// 			else
-// 				ft_printf("0");
-// 		}
-// 		i--;
-// 	}
-// }
-
-// int main(int argc, char **argv)
-// {
-// 	int				pid;
-// 	struct sigaction sigact;
-
-// 	pid = arg_check(argc, argv);
-// 	if (pid <= 0)
-// 		return (ft_printf("The pid is incorrect!"), 1);
-// 	handler_initializer(&sigact, pid, argv[2]);
-// 	return (0);
-// }
-
-static int	g_flag = 1;
-#define MASK 1
+t_cinfo	g_info;
 
 int	args_check(int ac, char **av)
 {
 	if (ac != 3)
-	{
-		ft_printf("Invalid number of args!\n(Syntax: %s process_id message)\n",
-			av[0]);
-		return (0);
-	}
+		return (ft_printf("%s", INVALID_ARGS_N), 0);
+	if (!*av[1] || !*av[2])
+		return (ft_printf("%s", EMPTY_ARG), 0);
+	ft_memset(&g_info, 0, sizeof(t_cinfo));
+	g_info.pid = ft_atoi(av[1]);
+	if (g_info.pid < 0)
+		return (ft_printf("%s", NEGATIVE_PID_ERROR), 0);
+	else if (g_info.pid == 0)
+		return (ft_printf("%s", PID_ERROR), 0);
 	return (1);
 }
 
-void	data_send(int pid, unsigned int byte, char *message)
+void	data_sender(unsigned int byte, char *message)
 {
 	int		i;
 
@@ -118,56 +38,41 @@ void	data_send(int pid, unsigned int byte, char *message)
 	{
 		if ((message[byte] >> i) & MASK)
 		{
-			if (kill(pid, SIGUSR1) != 0)
-				return (ft_printf("The pid is incorrect!\n"), exit(0));
+			if (kill(g_info.pid, SIGUSR1) != 0)
+				return (ft_printf("%s", PID_ERROR), exit(0));
 		}
 		else
 		{
-			if (kill(pid, SIGUSR2) != 0)
-				return (ft_printf("The pid is incorrect!\n"), exit(0));
+			if (kill(g_info.pid, SIGUSR2) != 0)
+				return (ft_printf("%s", PID_ERROR), exit(0));
 		}
 		i--;
-		while (g_flag)
-			usleep(10);
-		g_flag = 1;
+		while (g_info.flag)
+			usleep(5);
+		g_info.flag = 1;
 	}
 }
 
-void notifier(int sig)
+void	notifier(int sig)
 {
-	// ft_printf("handler\n");
 	if (sig == SIGUSR1)
-	{
-		ft_printf("p");
-		g_flag = 0;
-	}
-	// {
-	// 	ft_printf("%d", g_flag);
-	// }
+		g_info.flag = 0;
 	else if (sig == SIGUSR2)
 		ft_printf("MESSAGE HAS BEEN RECEIVED!");
 }
 
 int	main(int argc, char **argv)
 {
-	int					pid;
 	char				*message;
 	unsigned int		byte;
-	// struct sigaction	ts;
 
 	if (!args_check(argc, argv))
 		return (1);
 	signal(SIGUSR1, notifier);
 	signal(SIGUSR2, notifier);
-	// ts.sa_flags = 0;
-	// ts.sa_handler = notifier;
-	// sigaction(SIGUSR1);
-	// sigaction(SIGUSR2);
 	message = argv[2];
-	pid = ft_atoi(argv[1]); if (pid <= 0)
-		return (ft_printf("The pid is incorrect!\n"));
 	byte = -1;
 	while (message[++byte])
-		data_send(pid, byte, message);
+		data_sender(byte, message);
 	return (0);
 }
--- server_bonus.c
+++ server_bonus.c
@@ -12,64 +12,50 @@
 
 #include "minitalk_bonus.h"
 
-typedef struct s_info
-{
-	int		byte_sequence;
-	char	bytes[4];
-	char	octet;
-	int		current_byte;
-	int		current_bit;
-	int		last_pid;
-}	t_info;
+t_info	g_info;
 
-t_info	info;
-
-void	expected_bytes(t_info *info, unsigned char octet)
+void	expected_bytes(unsigned char octet)
 {
-	if (info->byte_sequence)
-		return ;
 	if ((octet & 0x80) == 0)
-		info->byte_sequence = 1;
+		g_info.byte_sequence = 1;
 	if ((octet & 0xE0) == 0xC0)
-		info->byte_sequence = 2;
+		g_info.byte_sequence = 2;
 	if ((octet & 0xF0) == 0xE0)
-		info->byte_sequence = 3;
+		g_info.byte_sequence = 3;
 	if ((octet & 0xF8) == 0xF0)
-		info->byte_sequence = 4;
+		g_info.byte_sequence = 4;
 }
 
-void	write_byte(int sig, siginfo_t *s, void *contest)
+void	byte_writer(void)
+{
+	write(1, &g_info.bytes, g_info.byte_sequence);
+	ft_memset(&g_info, 0, sizeof(t_info) - sizeof(int));
+}
+
+void	data_receiver(int sig, siginfo_t *s, void *contest)
 {
 	(void)contest;
-
-	if (s->si_pid != info.last_pid)
+	if (s->si_pid != g_info.last_pid)
 	{
-		ft_memset(&info, 0, sizeof(t_info));
-		info.current_bit = 7;
-		info.last_pid = s->si_pid;
+		ft_memset(&g_info, 0, sizeof(t_info));
+		g_info.current_bit = 7;
+		g_info.last_pid = s->si_pid;
 	}
 	if (sig == SIGUSR1)
-		info.octet |= 1 << info.current_bit;
-	info.current_bit--;
-	if (info.current_bit == -1)
+		g_info.octet |= 1 << g_info.current_bit;
+	g_info.current_bit--;
+	if (g_info.current_bit == -1)
 	{
-		if (info.octet == '\0')
-		{
-			int a = kill(s->si_pid, SIGUSR2);
-			ft_printf ("a ::  %d \n", a);
-			return ;
-		}
-		if (info.current_byte == 0)
-			expected_bytes(&info, info.octet);
-		info.bytes[info.current_byte] = info.octet;
-		info.current_byte++;
-		if (info.current_byte == info.byte_sequence)
-		{
-			write(1, &info.bytes, info.byte_sequence);
-			ft_memset(&info, 0, sizeof(t_info) - sizeof(int));
-		}
-		info.octet = 0;
-		info.current_bit = 7;
+		if (g_info.octet == '\0')
+			return (kill(s->si_pid, SIGUSR2), exit(0));
+		if (g_info.current_byte == 0)
+			expected_bytes(g_info.octet);
+		g_info.bytes[g_info.current_byte] = g_info.octet;
+		g_info.current_byte++;
+		if (g_info.current_byte == g_info.byte_sequence)
+			byte_writer();
+		g_info.octet = 0;
+		g_info.current_bit = 7;
 	}
 	kill(s->si_pid, SIGUSR1);
 }
@@ -78,15 +64,11 @@ void	sa_install(struct sigaction *sa_ptr)
 {
 	pid_t				pid;
 
-	info.byte_sequence = 0;
-	info.current_byte = 0;
-	info.current_bit = 7;
-	info.last_pid = 0;
 	pid = getpid();
 	ft_printf("%d\n", pid);
 	sigemptyset(&sa_ptr->sa_mask);
 	sa_ptr->sa_flags = SA_SIGINFO;
-	sa_ptr->sa_sigaction = write_byte;
+	sa_ptr->sa_sigaction = data_receiver;
 }
 
 int	main(void)
@@ -100,69 +82,3 @@ int	main(void)
 		pause();
 	return (0);
 }
-
-// #include <stdio.h>
-// void	write_bits(int *bit, char *octet, siginfo_t *sig_info)
-// {
-// 	// printf("\n%d", *bit);
-// 	if (*bit == -1)
-// 	{
-// 		write(1, octet, 1);
-// 		if (*octet == '\0')
-// 			kill(sig_info->si_pid, SIGUSR2);
-// 		*bit = 7;
-// 		*octet = 0;
-// 	}
-// 	kill(sig_info->si_pid, SIGUSR1);
-// }
-
-// void	handler(int sig, siginfo_t *sig_info, void *ptr)
-// {
-// 	static int	bit;
-// 	static char octet;
-// 	(void)octet;
-// 	(void)bit;
-// 	(void)ptr;
-
-// 	bit = 7;
-// 	octet = 0;
-// 	if (sig_info->si_pid != info.last_pid)
-// 	{
-// 		bit = 7;
-// 		octet = 0;
-// 	}
-// 	info.last_pid = sig_info->si_pid;
-// 	printf("here");
-// 	if (sig == SIGUSR1)
-// 		printf("sigusr1");
-// 	if (sig == SIGUSR2)
-// 		printf("sigusr2");
-// 	// bit--;
-// 	// printf("\n%d", bit);
-// 	// if (bit == -1)
-// 	// 	info.current_byte++;
-// 	// write_bits(&bit, &octet, sig_info);
-// }
-
-// void handerl_initializer(struct sigaction *sa_ptr)
-// {
-// 	pid_t				pid;
-
-// 	pid = getpid();
-// 	ft_printf("%d\n", pid);
-// 	sigemptyset(&sa_ptr->sa_mask);
-// 	sa_ptr->sa_flags = SA_SIGINFO;
-// 	sa_ptr->sa_sigaction = handler;
-// }
-
-// int main()
-// {
-// 	struct sigaction sigact;
-
-// 	handerl_initializer(&sigact);
-// 	sigaction(SIGUSR1, &sigact, NULL);
-// 	sigaction(SIGUSR2, &sigact, NULL);
-// 	while (1)
-// 		pause();
-// 	return (0);
-// }
